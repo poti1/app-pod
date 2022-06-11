23702e9036e6d138cf976035506634640173e23d
Timofey Potapov <timofey.potapov@otrs.com>
2022-06-11 22:56:17 +0200

    0.05 - Tidy

--- lib/App/Pod.pm
+++ lib/App/Pod.pm
@@ -32,11 +32,11 @@ App::Pod - Quickly show available class methods and documentation.
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
@@ -89,50 +89,51 @@ Run the main program.
 =cut
 
 sub run {
-   my $self = __PACKAGE__->new;
-   my $opts = get_opts();
+    my $self = __PACKAGE__->new;
+    my $opts = get_opts();
 
-   if ( $opts->{list_tool_options} ) {
-      list_tool_options();
-      return unless $opts->{list_class_options};
-   }
+    if ( $opts->{list_tool_options} ) {
+        list_tool_options();
+        return unless $opts->{list_class_options};
+    }
 
-   if ( not @ARGV or $opts->{help} ) {
-      show_help();
-      return;
-   }
+    if ( not @ARGV or $opts->{help} ) {
+        show_help();
+        return;
+    }
 
-   my ( $class, @args ) = @ARGV;
-   my ( $method ) = @args;
+    my ( $class, @args ) = @ARGV;
+    my ( $method ) = @args;
 
-   if ( $opts->{list_class_options} ) {
-      return if $self->list_class_options( $class );
-   }
+    if ( $opts->{list_class_options} ) {
+        return if $self->list_class_options( $class );
+    }
 
-   import_class( $class ) or return;
+    import_class( $class ) or return;
 
-   if ( $opts->{edit} ) {
-      edit_file( $class, $method );
-   }
-   elsif ( $opts->{doc} ) {
-      doc_class( $class, @args );
-   }
+    if ( $opts->{edit} ) {
+        edit_file( $class, $method );
+    }
+    elsif ( $opts->{doc} ) {
+        doc_class( $class, @args );
+    }
 
-   print_header( $class );
-   if ( $method ) {
-      show_method_doc( $class, $method );
-   }
-   else {
-      show_inheritance( $class );
-      my $save = {
-         class   => $class,
-         options => [ show_events( $class ), show_methods( $class, $opts ), ],
-      };
-      save_last_class_and_options( $save );
-      if ( $opts->{list_class_options} ) {
-         return if $self->list_class_options( $class );
-      }
-   }
+    print_header( $class );
+    if ( $method ) {
+        show_method_doc( $class, $method );
+    }
+    else {
+        show_inheritance( $class );
+        my $save = {
+            class   => $class,
+            options =>
+              [ show_events( $class ), show_methods( $class, $opts ), ],
+        };
+        save_last_class_and_options( $save );
+        if ( $opts->{list_class_options} ) {
+            return if $self->list_class_options( $class );
+        }
+    }
 }
 
 =head2 new
@@ -142,12 +143,12 @@ Create a new object.
 =cut
 
 sub new {
-   my ( $class ) = @_;
-   my $null_fh;
+    my ( $class ) = @_;
+    my $null_fh;
 
-   bless {
-      null_fh => \$null_fh,    # TODO: WHY?
-   }, $class;
+    bless {
+        null_fh => \$null_fh,    # TODO: WHY?
+    }, $class;
 }
 
 =head2 define_spec
@@ -157,7 +158,7 @@ Define the command line argument specificaiton.
 =cut
 
 sub define_spec {
-   <<~SPEC;
+    <<~SPEC;
 
       all|a              - Show all class functions.
       doc|d              - View the class documentation.
@@ -170,28 +171,28 @@ sub define_spec {
 }
 
 sub _build_spec_list {
-   map    { [ split / \s+ - \s+ /x, $_, 2 ] }   # Split into: opts - description
-     map  { b( $_ )->trim }                     # Trim leading/trailing spaces
-     grep { not /^ \s* $/x }                    # Blank lines
-     split "\n", define_spec();
+    map    { [ split / \s+ - \s+ /x, $_, 2 ] }  # Split into: opts - description
+      map  { b( $_ )->trim }                    # Trim leading/trailing spaces
+      grep { not /^ \s* $/x }                   # Blank lines
+      split "\n", define_spec();
 }
 
 sub get_spec_list {
-   map { $_->[0] } _build_spec_list();
+    map { $_->[0] } _build_spec_list();
 }
 
 sub get_optios_list {
-   sort
-     map { length( $_ ) == 1 ? "-$_" : "--$_"; }
-     map { split /\|/ } get_spec_list();
+    sort
+      map { length( $_ ) == 1 ? "-$_" : "--$_"; }
+      map { split /\|/ } get_spec_list();
 }
 
 sub get_opts {
-   my $opts = {};
+    my $opts = {};
 
-   GetOptions( $opts, get_spec_list ) or die $!;
+    GetOptions( $opts, get_spec_list ) or die $!;
 
-   $opts;
+    $opts;
 }
 
 =head2 list_tool_options
@@ -202,7 +203,7 @@ to this tool.
 =cut
 
 sub list_tool_options {
-   say for get_optios_list();
+    say for get_optios_list();
 }
 
 =head2 list_class_options
@@ -212,38 +213,38 @@ sub list_tool_options {
 =cut
 
 sub list_class_options {
-   my ( $self, $class ) = @_;
-   my $last_data = get_last_class_and_options();
-   if ( $last_data->{class} eq $class ) {
-      select STDOUT;
-      say for $last_data->{options}->@*;
-      return 1;
-   }
+    my ( $self, $class ) = @_;
+    my $last_data = get_last_class_and_options();
+    if ( $last_data->{class} eq $class ) {
+        select STDOUT;
+        say for $last_data->{options}->@*;
+        return 1;
+    }
 
-   # Ignore the output.
-   open $self->{null_fh}, '>', '/dev/null' or die $!;
-   $self->{stdout_fh} = select $self->{null_fh};
+    # Ignore the output.
+    open $self->{null_fh}, '>', '/dev/null' or die $!;
+    $self->{stdout_fh} = select $self->{null_fh};
 }
 
 sub show_help {
-   my $scipt = _yellow( "pod" );
+    my $scipt = _yellow( "pod" );
 
-   my @all = map {
-      my ( $opt, $desc ) = @$_;
-      $opt =~ s/\|/, /g;
-      $opt =~ s/ (?=\b\w{2}) /--/gx;    # Long opts
-      $opt =~ s/ (?=\b\w\b)  /-/gx;     # Short opts
-      my $colored_opt = _green( $opt );
-      [ $colored_opt, _grey( $desc ), length $colored_opt ];
-   } _build_spec_list();
+    my @all = map {
+        my ( $opt, $desc ) = @$_;
+        $opt =~ s/\|/, /g;
+        $opt =~ s/ (?=\b\w{2}) /--/gx;    # Long opts
+        $opt =~ s/ (?=\b\w\b)  /-/gx;     # Short opts
+        my $colored_opt = _green( $opt );
+        [ $colored_opt, _grey( $desc ), length $colored_opt ];
+    } _build_spec_list();
 
-   my $max = max map { $_->[2] } @all;
+    my $max = max map { $_->[2] } @all;
 
-   my $options =
-     join "\n   ",
-     map { sprintf "%-${max}s - %s", @$_[ 0, 1 ] } @all;
+    my $options =
+      join "\n   ",
+      map { sprintf "%-${max}s - %s", @$_[ 0, 1 ] } @all;
 
-   say <<~HELP;
+    say <<~HELP;
 
    @{[ _grey("Shows available class methods and documentation") ]}
 
@@ -274,269 +275,271 @@ sub show_help {
 }
 
 sub import_class {
-   my ( $class ) = @_;
+    my ( $class ) = @_;
 
-   # Since ojo imports its DSL into the current package
-   eval { eval "package $class; use $class"; };
+    # Since ojo imports its DSL into the current package
+    eval { eval "package $class; use $class"; };
 
-   my $import_ok = do {
-      if ( $@ ) { warn $@; 0 }
-      else      { 1 }
-   };
+    my $import_ok = do {
+        if ( $@ ) { warn $@; 0 }
+        else      { 1 }
+    };
 
-   $import_ok;
+    $import_ok;
 }
 
 sub edit_file {
-   my ( $class, $method ) = @_;
-   my $path = Pod::Query->new( $class, "path" )->path;
-   my $cmd  = "vim $path";
+    my ( $class, $method ) = @_;
+    my $path = Pod::Query->new( $class, "path" )->path;
+    my $cmd  = "vim $path";
 
-   if ( $method ) {
-      my $m      = "<\\zs$method\\ze>";
-      my $sub    = "<sub $m";
-      my $monkey = "<monkey_patch>.+$m";
-      my $list   = "^ +$m +\\=\\>";
-      my $qw     = "<qw>.+$m";
-      my $emit   = "<(emit|on)\\($m";
-      $cmd .= " '+/\\v$sub|$monkey|$list|$qw|$emit'";
-   }
+    if ( $method ) {
+        my $m      = "<\\zs$method\\ze>";
+        my $sub    = "<sub $m";
+        my $monkey = "<monkey_patch>.+$m";
+        my $list   = "^ +$m +\\=\\>";
+        my $qw     = "<qw>.+$m";
+        my $emit   = "<(emit|on)\\($m";
+        $cmd .= " '+/\\v$sub|$monkey|$list|$qw|$emit'";
+    }
 
-   # say $cmd;
-   # exit;
-   exec $cmd;
+    # say $cmd;
+    # exit;
+    exec $cmd;
 }
 
 sub doc_class {
-   my ( $class, @args ) = @_;
-   my $cmd = "perldoc @args $class";
+    my ( $class, @args ) = @_;
+    my $cmd = "perldoc @args $class";
 
-   # say $cmd;
-   exec $cmd;
+    # say $cmd;
+    exec $cmd;
 }
 
 sub print_header {
-   my ( $class )     = @_;
-   my $pod           = Pod::Query->new( $class );
-   my $version       = $class->VERSION;
-   my $first_release = Module::CoreList->first_release( $class );
+    my ( $class )     = @_;
+    my $pod           = Pod::Query->new( $class );
+    my $version       = $class->VERSION;
+    my $first_release = Module::CoreList->first_release( $class );
 
-   my @package_line = (
-      _grey( "Package:" ),
-      sprintf(
-         "%s%s%s",
-         _yellow( $class ),
-         ( $version ? _green( " $version" ) : "" ),
-         (
-            $first_release
-            ? _grey( " (since perl " ) . _green( $first_release ) . _grey( ")" )
-            : ""
-         ),
-      ),
-   );
-   my @path_line = ( _grey( "Path:" ), _grey( $pod->path ), );
+    my @package_line = (
+        _grey( "Package:" ),
+        sprintf(
+            "%s%s%s",
+            _yellow( $class ),
+            ( $version ? _green( " $version" ) : "" ),
+            (
+                $first_release
+                ? _grey( " (since perl " )
+                  . _green( $first_release )
+                  . _grey( ")" )
+                : ""
+            ),
+        ),
+    );
+    my @path_line = ( _grey( "Path:" ), _grey( $pod->path ), );
 
-   my $max    = max map { length } $package_line[0], $path_line[0];
-   my $format = "%-${max}s %s";
+    my $max    = max map { length } $package_line[0], $path_line[0];
+    my $format = "%-${max}s %s";
 
-   say "";
-   sayt sprintf( $format, @package_line );
-   sayt sprintf( $format, @path_line );
+    say "";
+    sayt sprintf( $format, @package_line );
+    sayt sprintf( $format, @path_line );
 
-   say "";
-   my ( $name, $summary ) = split /\s*-\s*/, $pod->find_title, 2;
-   return unless $name and $summary;
+    say "";
+    my ( $name, $summary ) = split /\s*-\s*/, $pod->find_title, 2;
+    return unless $name and $summary;
 
-   sayt _yellow( $name ) . " - " . _green( $summary );
-   say "";
+    sayt _yellow( $name ) . " - " . _green( $summary );
+    say "";
 }
 
 sub show_method_doc {
-   my ( $class, $method ) = @_;
-   my $doc = Pod::Query->new( $class )->find_method( $method );
+    my ( $class, $method ) = @_;
+    my $doc = Pod::Query->new( $class )->find_method( $method );
 
-   for ( $doc ) {
+    for ( $doc ) {
 
-      # Headings.
-      s/ ^ \s* \K (\S+:) (?= \s* $ ) / _green($1) /xgem;
+        # Headings.
+        s/ ^ \s* \K (\S+:) (?= \s* $ ) / _green($1) /xgem;
 
-      # Comments.
-      s/ (\#.+) / _grey($1) /xge;
-   }
+        # Comments.
+        s/ (\#.+) / _grey($1) /xge;
+    }
 
-   say $doc;
+    say $doc;
 }
 
 sub show_inheritance {
-   my ( @classes ) = @_;
-   my @tree;
-   my %seen;
-   no strict 'refs';
+    my ( @classes ) = @_;
+    my @tree;
+    my %seen;
+    no strict 'refs';
 
-   while ( my $class = shift @classes ) {
-      next if $seen{class};    # Already saw it
-      $seen{$class}++;         # Otherwise, now we did
-      push @tree, $class;      # Add to tree
+    while ( my $class = shift @classes ) {
+        next if $seen{class};    # Already saw it
+        $seen{$class}++;         # Otherwise, now we did
+        push @tree, $class;      # Add to tree
 
-      eval "require $class";
-      my @isa = @{"${class}::ISA"};
-      push @classes, @isa;
-   }
+        eval "require $class";
+        my @isa = @{"${class}::ISA"};
+        push @classes, @isa;
+    }
 
-   my $size = @tree;
-   return if $size <= 1;
-   say _neon( "Inheritance ($size):" );
-   say _grey( " $_" ) for @tree;
-   say "";
+    my $size = @tree;
+    return if $size <= 1;
+    say _neon( "Inheritance ($size):" );
+    say _grey( " $_" ) for @tree;
+    say "";
 }
 
 sub show_events {
-   my ( $class ) = @_;
-   my %events    = Pod::Query->new( $class )->find_events;
-   my @names     = sort keys %events;
-   my $size      = @names;
-   return unless $size;
+    my ( $class ) = @_;
+    my %events    = Pod::Query->new( $class )->find_events;
+    my @names     = sort keys %events;
+    my $size      = @names;
+    return unless $size;
 
-   my @save;
-   my $len    = max map { length( _green( $_ ) ) } @names;
-   my $format = " %-${len}s - %s";
+    my @save;
+    my $len    = max map { length( _green( $_ ) ) } @names;
+    my $format = " %-${len}s - %s";
 
-   say _neon( "Events ($size):" );
-   for ( @names ) {
-      sayt sprintf $format, _green( $_ ), _grey( $events{$_} );
-      push @save, $_;
-   }
+    say _neon( "Events ($size):" );
+    for ( @names ) {
+        sayt sprintf $format, _green( $_ ), _grey( $events{$_} );
+        push @save, $_;
+    }
 
-   say "";
+    say "";
 
-   @save;
+    @save;
 }
 
 sub show_methods {
-   my ( $class, $opts ) = @_;
+    my ( $class, $opts ) = @_;
 
-   #my @dirs = $class->dir;
-   my @dirs = sort { $a cmp $b } get_full_functions( $class );
-   my $pod  = Pod::Query->new( $class );
-   my $doc  = "";
+    #my @dirs = $class->dir;
+    my @dirs = sort { $a cmp $b } get_full_functions( $class );
+    my $pod  = Pod::Query->new( $class );
+    my $doc  = "";
 
-   my @meths_all = map {
-      my $doc = $pod->find_method_summary( $_ );
-      [ $_, $doc ];
-   } @dirs;
+    my @meths_all = map {
+        my $doc = $pod->find_method_summary( $_ );
+        [ $_, $doc ];
+    } @dirs;
 
-   # Documented methods
-   my @meths_doc = grep { $_->[1] } @meths_all;
-   my @save =
-     grep { / ^ [\w_-]+ $ /x }
-     map { $_->[0] } @meths_all;
+    # Documented methods
+    my @meths_doc = grep { $_->[1] } @meths_all;
+    my @save =
+      grep { / ^ [\w_-]+ $ /x }
+      map { $_->[0] } @meths_all;
 
-   # If we have methods, but none are documented
-   if ( @meths_all and not @meths_doc ) {
-      say _grey(
-         "Warning: All methods are undocumented! (reverting to --all)\n" );
-      $opts->{all} = 1;
-   }
+    # If we have methods, but none are documented
+    if ( @meths_all and not @meths_doc ) {
+        say _grey(
+            "Warning: All methods are undocumented! (reverting to --all)\n" );
+        $opts->{all} = 1;
+    }
 
-   my @meths = $opts->{all} ? @meths_all : @meths_doc;
-   my $size  = @meths;
-   my $max   = max map { length _green( $_->[0] ) } @meths;
-   $max //= 0;
+    my @meths = $opts->{all} ? @meths_all : @meths_doc;
+    my $size  = @meths;
+    my $max   = max map { length _green( $_->[0] ) } @meths;
+    $max //= 0;
 
-   my $format = " %-${max}s%s";
-   say _neon( "Methods ($size):" );
+    my $format = " %-${max}s%s";
+    say _neon( "Methods ($size):" );
 
-   for my $list ( @meths ) {
-      my ( $method, $doc_raw ) = @$list;
-      my $doc = $doc_raw ? " - $doc_raw" : "";
-      $doc =~ s/\n+/ /g;
-      sayt sprintf $format, _green( $method ), _grey( $doc );
-   }
+    for my $list ( @meths ) {
+        my ( $method, $doc_raw ) = @$list;
+        my $doc = $doc_raw ? " - $doc_raw" : "";
+        $doc =~ s/\n+/ /g;
+        sayt sprintf $format, _green( $method ), _grey( $doc );
+    }
 
-   say _grey( "\nUse --all (or -a) to see all methods." )
-     unless $opts->{all};
-   say "";
+    say _grey( "\nUse --all (or -a) to see all methods." )
+      unless $opts->{all};
+    say "";
 
-   @save;
+    @save;
 }
 
 sub _trim {
-   my ( $line )    = @_;
-   my $term_width  = Pod::Query::get_term_width();
-   my $replacement = " ...";
-   my $width = $term_width - length( $replacement ) - 1;    # "-1" for newline
+    my ( $line )    = @_;
+    my $term_width  = Pod::Query::get_term_width();
+    my $replacement = " ...";
+    my $width = $term_width - length( $replacement ) - 1;    # "-1" for newline
 
-   # Trim to terminal width
-   my $colored_length   = length( $line );
-   my $uncolored_length = length( colorstrip( $line ) );
-   my $diff_length      = $colored_length - $uncolored_length;
-   $diff_length = $diff_length - 3 if $diff_length;
+    # Trim to terminal width
+    my $colored_length   = length( $line );
+    my $uncolored_length = length( colorstrip( $line ) );
+    my $diff_length      = $colored_length - $uncolored_length;
+    $diff_length = $diff_length - 3 if $diff_length;
 
-   if ( $uncolored_length >= $term_width ) {    # "=" also for newline
-      $line = substr( $line, 0, $width + $diff_length ) . $replacement;
-   }
+    if ( $uncolored_length >= $term_width ) {    # "=" also for newline
+        $line = substr( $line, 0, $width + $diff_length ) . $replacement;
+    }
 
-   $line;
+    $line;
 }
 
 sub r {
 
-   say dumper \@_;
+    say dumper \@_;
 }
 
 sub sayt {
 
-   say _trim( @_ );
+    say _trim( @_ );
 }
 
 sub _red {
 
-   colored( "@_", "RESET RED" );
+    colored( "@_", "RESET RED" );
 }
 
 sub _yellow {
 
-   colored( "@_", "RESET YELLOW" );
+    colored( "@_", "RESET YELLOW" );
 }
 
 sub _green {
 
-   # Reset since last line may be trimmed.
-   colored( "@_", "RESET GREEN" );
+    # Reset since last line may be trimmed.
+    colored( "@_", "RESET GREEN" );
 }
 
 sub _grey {
 
-   colored( "@_", "RESET DARK" );
+    colored( "@_", "RESET DARK" );
 }
 
 sub _neon {
 
-   colored( "@_", "RESET ON_BRIGHT_BLACK" );
+    colored( "@_", "RESET ON_BRIGHT_BLACK" );
 }
 
 sub define_last_run_cache_file {
-   catfile( $ENV{HOME}, ".cache", "my_pod_last_run.cache" );
+    catfile( $ENV{HOME}, ".cache", "my_pod_last_run.cache" );
 
 }
 
 sub save_last_class_and_options {
-   my ( $save ) = @_;
-   my $file     = define_last_run_cache_file();
-   my $path     = path( $file );
+    my ( $save ) = @_;
+    my $file     = define_last_run_cache_file();
+    my $path     = path( $file );
 
-   if ( not -e $path->dirname ) {
-      mkdir $path->dirname or die $!;
-   }
+    if ( not -e $path->dirname ) {
+        mkdir $path->dirname or die $!;
+    }
 
-   $path->spurt( j $save );
+    $path->spurt( j $save );
 }
 
 sub get_last_class_and_options {
-   my $file = define_last_run_cache_file();
-   return { class => '' } unless -e $file;
+    my $file = define_last_run_cache_file();
+    return { class => '' } unless -e $file;
 
-   j path( $file )->slurp;
+    j path( $file )->slurp;
 }
 
 =for REMOVE
--- old/dir.pl
+++ old/dir.pl
@@ -3,43 +3,43 @@
 use v5.32;
 
 sub UNIVERSAL::dir {
-   my ( $s ) = @_;                  # class or object
-   my $ref   = ref $s;
-   my $class = $ref ? $ref : $s;    # myClass
-   my $pkg   = $class . "::";       # MyClass::
+    my ( $s ) = @_;                  # class or object
+    my $ref   = ref $s;
+    my $class = $ref ? $ref : $s;    # myClass
+    my $pkg   = $class . "::";       # MyClass::
 
-   no strict 'refs';
-   my @keys =
-     grep { defined $pkg->{$_}->*{CODE} }
-     sort keys %$pkg;
+    no strict 'refs';
+    my @keys =
+      grep { defined $pkg->{$_}->*{CODE} }
+      sort keys %$pkg;
 
-   return @keys if defined wantarray;
+    return @keys if defined wantarray;
 
-   local $" = ', ';                 # join separator
-   print "$class: [@keys]";
+    local $" = ', ';                 # join separator
+    print "$class: [@keys]";
 }
 
 package MyClass {    # Sample class
-   sub new   { bless {}, shift }
-   sub func1 { }
-   sub func2 { }
+    sub new   { bless {}, shift }
+    sub func1 { }
+    sub func2 { }
 }
 
 sub test {
-   MyClass->dir;    # MyClass: [func1, func2, new]
+    MyClass->dir;    # MyClass: [func1, func2, new]
 
-   my $obj = MyClass->new;
-   $obj->dir;       # MyClass: [func1, func2, new]
+    my $obj = MyClass->new;
+    $obj->dir;       # MyClass: [func1, func2, new]
 
-   print for $obj->dir;    # [func1, func2, new]
+    print for $obj->dir;    # [func1, func2, new]
 
-   # (ref($obj) . "::")->{var}->*{SCALAR}->$* = 42;
-   # (ref($obj) . "::")->{var}->$* = 42;
-   # print $MyClass::var;
+    # (ref($obj) . "::")->{var}->*{SCALAR}->$* = 42;
+    # (ref($obj) . "::")->{var}->$* = 42;
+    # print $MyClass::var;
 }
 
 if ( @ARGV ) {
-   my $class = shift;
-   eval "require $class" or die $@;
-   $class->dir;
+    my $class = shift;
+    eval "require $class" or die $@;
+    $class->dir;
 }
