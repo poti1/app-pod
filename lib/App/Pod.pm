1321c10ad8594cf30d6f565d98cb4db812f3d637
Timofey Potapov <timofey.potapov@otrs.com>
2022-06-26 06:26:40 +0200

    0.17 - Using File::HomeDir in build script.

--- lib/App/Pod.pm
+++ lib/App/Pod.pm
@@ -51,11 +51,11 @@ App::Pod - Quickly show available class methods and documentation.
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 =cut
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 
 =head1 SYNOPSIS
