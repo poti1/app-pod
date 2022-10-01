2dac2b8842d05b5cdbce0055ec0ac1a210682d78
Timofey Potapov <timofey.potapov@otrs.com>
2022-10-01 18:03:30 +0200

    +0.28    2022-10-01 - Updated Changes  0.27    2022-10-01 - Added bugtracker resources. +        Removed File::HomeDir as a Build.PL dependancy.

--- lib/App/Pod.pm
+++ lib/App/Pod.pm
@@ -30,11 +30,11 @@ App::Pod - Quickly show available class methods and documentation.
 
 =head1 VERSION
 
-Version 0.27
+Version 0.28
 
 =cut
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 
 
 =head1 SYNOPSIS
